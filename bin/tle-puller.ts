b738fd1cd369ca01d5f5322ae9164cc41c361263
Mitchell Loeppky <mrl@amazon.com>
2021-02-02 08:46:38 -0800

    Create pull-tle script to fetch data from spack-track.

---
+++ bin/tle-puller.ts
@@ -0,0 +1,77 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+import credentials from '../spaceTrackCreds.json';
+
+import SpaceTrack from '../src/SpaceTrack';
+
+const getDayStringFromDate = (date: Date): string => {
+    return date.toISOString().split('T')[0];
+};
+
+const getDateFromDayString = (dayString: string): Date => {
+    const parts = dayString.split('-');
+    return new Date(Date.UTC(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]), 12, 0, 0, 0));
+};
+
+const incrementOneDay = (date: Date): Date => {
+    return new Date(date.getTime() + 24 * 60 * 60 * 1000);
+};
+
+const incrementDayStringOneDay = (dateString: string): string => {
+    const inputDate = getDateFromDayString(dateString);
+    const outputDate = incrementOneDay(inputDate);
+    return getDayStringFromDate(outputDate);
+};
+
+const findNextDateToPull = (startDayString: string, endDayString: string, existingFiles: string[]): string => {
+    let curDayString = startDayString;
+    while (existingFiles.includes(curDayString + '.csv.gz') && curDayString !== endDayString) {
+        curDayString = incrementDayStringOneDay(curDayString);
+    }
+    return curDayString;
+};
+
+const START_DATE = '1959-02-17'; // Launch of Vanguard 2. Sputnik 1 was launched '1957-12-04' but I do not have good data for satellites before Vanguard 2.
+const END_DATE = getDayStringFromDate(new Date());
+
+const spaceTrack = new SpaceTrack(credentials);
+
+const run = async (): Promise<void> => {
+    const files = fs.readdirSync('resources/raw');
+    // console.log(files);
+
+    let curDayString = START_DATE;
+    const pullNextFile = async () => {
+        curDayString = findNextDateToPull(curDayString, END_DATE, files);
+        const nextDayString = incrementDayStringOneDay(curDayString);
+        console.log(`Starting fetching data from ${curDayString}`);
+        const csv = await spaceTrack.getTLEsForDateRange(curDayString, nextDayString);
+        let numResults = 0;
+        if (csv !== 'NO RESULTS RETURNED') {
+            numResults = csv.split('\n').length;
+        }
+        console.log(`Retrieved ${numResults} results`);
+        console.log('Gzipping data');
+        const sizeBeforeGzip = csv.length;
+        const gzipped = zlib.gzipSync(csv);
+        const sizeAfterGzip = gzipped.length;
+        const reduction = Math.round((-(sizeBeforeGzip - sizeAfterGzip) / sizeBeforeGzip) * 100);
+        console.log(`Gzip results: before = ${sizeBeforeGzip}, after = ${sizeAfterGzip} (${reduction}%)`);
+        const filePath = `resources/raw/${curDayString}.csv.gz`;
+        console.log(`Writing data to ${filePath}`);
+        fs.writeFileSync(filePath, csv);
+        console.log(`Done processing ${curDayString}`);
+        console.log('===========================\n');
+        curDayString = nextDayString;
+    };
+    setInterval(pullNextFile, 15 * 1000);
+    await pullNextFile();
+
+    // await new SpaceTrack(credentials).getTLEsForDateRange('2020-01-01', '2020-01-08');
+    // console.log(await new SpaceTrack(credentials).getTLEsForDateRange('1958-02-01', '1959-09-26'));
+
+    // await new SpaceTrack(credentials).getTLEsForDateRange(curDayString, incrementDayStringOneDay(curDayString));
+};
+
+run().then().catch(console.error);
---
+++ src/SpaceTrack.ts
@@ -0,0 +1,81 @@
+import request, { CookieJar } from 'request';
+
+interface Credentials {
+    username: string;
+    password: string;
+}
+
+export default class SpaceTrack {
+    private static readonly BASE_URL = 'https://www.space-track.org';
+    private readonly credentials: Credentials;
+    private readonly cookieJar = request.jar();
+    private activeLoginAttempt = Promise.resolve();
+
+    constructor(credentials: Credentials) {
+        this.credentials = credentials;
+    }
+
+    public async getTLEsForDateRange(startDayString: string, endDayString: string): Promise<string> {
+        await this.loginIfNeeded();
+
+        return new Promise((resolve, reject) => {
+            const url = `${SpaceTrack.BASE_URL}/basicspacedata/query/class/gp_history/EPOCH/${startDayString}--${endDayString}/orderby/EPOCH/emptyresult/show/format/csv`;
+
+            request(
+                url,
+                {
+                    jar: this.cookieJar,
+                },
+                (err, res, body) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    if (res.statusCode !== 200) {
+                        return reject(`Failed to fetch from SpaceTrack. Status code: ${res.statusCode}`);
+                    }
+
+                    // console.log(body);
+                    return resolve(body);
+                },
+            );
+        });
+    }
+
+    private async loginIfNeeded(): Promise<void> {
+        // If we are already logging in, then wait for that login attempt
+        await this.activeLoginAttempt;
+
+        // If we already have a valid login cookie, return early
+        // console.log(this.cookieJar.getCookies(SpaceTrack.BASE_URL));
+        const spaceTrackCookies = this.cookieJar.getCookies(SpaceTrack.BASE_URL);
+        if (spaceTrackCookies.length > 0 && Date.now() + 1000 < (spaceTrackCookies[0].expires as Date).getTime()) {
+            return Promise.resolve();
+        }
+
+        // Login
+        console.log('Logging into SpaceTrack');
+        this.activeLoginAttempt = new Promise((resolve, reject) => {
+            request.post(
+                SpaceTrack.BASE_URL + '/ajaxauth/login',
+                {
+                    form: {
+                        identity: this.credentials.username,
+                        password: this.credentials.password,
+                    },
+                    jar: this.cookieJar,
+                    json: true,
+                },
+                (err, res, body) => {
+                    if (err) {
+                        return reject(err);
+                    } else if (res.statusCode !== 200) {
+                        return reject(body);
+                    }
+                    return resolve();
+                },
+            );
+        });
+        return this.activeLoginAttempt;
+    }
+}
